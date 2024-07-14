880fcca8678c8202b917de793fd2e9c7d214fdb5
lue-lue-lue-1 <2928559191@qq.com>
2024-07-14 14:30:27 +0800

    dffdfdf

--- WeChat/Client/Java_File/WeChat/Chat_Info.java
+++ WeChat/Client/Java_File/WeChat/Chat_Info.java
@@ -19,6 +19,8 @@ public class Chat_Info extends Box{
         textPane.setEditable(false);
         textPane.setFont(this.font);
         textPane.setBackground(Color.GREEN);
+        jskkkkkkkkkkkkkkkkljel
+        skjskljsl 
         FontMetrics fontMetrics = textPane.getFontMetrics(font);
         String[] chat_text_list = chat_text.split("\r\n|\r|\n");
         StringBuilder chat_textBuilder = new StringBuilder();
